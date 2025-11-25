e52599c048b68e86a419fc6ac0bb9a65e4124cd9
kohei-miyadai07 <wm25gb87@gmail.com>
2025-11-25 22:35:58 +0900

    JEP 507

---
+++ src/jp/ne/zaq/jcom/JEP_507_Primitive_Types_in_Patterns.java
@@ -0,0 +1,27 @@
+package jp.ne.zaq.jcom;
+
+public class JEP_507_Primitive_Types_in_Patterns {
+
+	public static void process(Object o) {
+		// 'o' が Integer 型かつ値が 100 の場合に特別な処理を行う
+		String result = switch (o) {
+		    // プリミティブ型パターン (Third Preview)
+		    case Integer i when i == 100 -> "Special int: 100";
+			case Integer i -> "int: " + i;
+			case Double d -> "double: " + d;
+			case Boolean b -> "boolean: " + b;
+			case String s -> "String: " + s;
+			default -> "unknown type"; 
+		};
+		IO.println(result);
+	}
+
+	public static void main() {
+		process(100);          // Special int: 100
+		process(42);           // int: 42
+		process(3.14);         // double: 3.14
+		process(true);         // boolean: true
+		process("Hello");      // String: Hello
+		process(new Object()); // unknown type
+	}
+}
